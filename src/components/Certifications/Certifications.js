36eb04210a1dfa0d59a77b6848522aef41056666
Tomas Ninno <tomasninno@hotmail.com>
2023-12-29 00:48:03 -0300

    corrections

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Loader from '../Loader/Loader'
 import "../Certifications/Certifications.css";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-
+/* import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+ */
 const certificationsData = [
   {
     id: 'cert-1',
